81820221e806254e0d23a794d0ab909abd05a441
Unknown <lean1998@hotmail.com>
2018-06-21 18:59:09 -0300

    Pause Menu

---
+++ MoonLander/Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsManager : MonoBehaviour
+{
+    public GameObject ResumeButton;
+    public GameObject BackToMainMenuButton;
+    public GameObject LeftAndRightArrowsImage;
+    public GameObject UpArrowAndSpace;
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        ResumeButton.SetActive(true);
+        BackToMainMenuButton.SetActive(true);
+        LeftAndRightArrowsImage.SetActive(true);
+        UpArrowAndSpace.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        ResumeButton.SetActive(false);
+        BackToMainMenuButton.SetActive(false);
+        LeftAndRightArrowsImage.SetActive(false);
+        UpArrowAndSpace.SetActive(false);
+    }
+
+    public void Test()
+    {
+        Debug.Log("TEST");
+    }
+
+}
