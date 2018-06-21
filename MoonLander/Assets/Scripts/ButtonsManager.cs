0c80aa82983b208b7c66f862c73dc172ab19b25d
Unknown <lean1998@hotmail.com>
2018-06-21 19:44:59 -0300

    UI Aspect ratio

--- MoonLander/Assets/Scripts/ButtonsManager.cs
+++ MoonLander/Assets/Scripts/ButtonsManager.cs
@@ -1,30 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ButtonsManager : MonoBehaviour
 {
+    public GameObject PauseButton;
     public GameObject ResumeButton;
     public GameObject BackToMainMenuButton;
     public GameObject LeftAndRightArrowsImage;
     public GameObject UpArrowAndSpace;
+    public Text RotateShip;
+    public Text ActivatePropulsion;
+    void Start()
+    {
+        PauseButton.SetActive(true);
+        ResumeButton.SetActive(false);
+        BackToMainMenuButton.SetActive(false);
+        LeftAndRightArrowsImage.SetActive(false);
+        UpArrowAndSpace.SetActive(false);
+        RotateShip.enabled = false;
+        ActivatePropulsion.enabled = false;
+    }
 
     public void Pause()
     {
         Time.timeScale = 0;
+        PauseButton.SetActive(false);
         ResumeButton.SetActive(true);
         BackToMainMenuButton.SetActive(true);
         LeftAndRightArrowsImage.SetActive(true);
         UpArrowAndSpace.SetActive(true);
+        RotateShip.enabled = true;
+        ActivatePropulsion.enabled = true;
     }
 
     public void Resume()
     {
         Time.timeScale = 1;
+        PauseButton.SetActive(true);
         ResumeButton.SetActive(false);
         BackToMainMenuButton.SetActive(false);
         LeftAndRightArrowsImage.SetActive(false);
         UpArrowAndSpace.SetActive(false);
+        RotateShip.enabled = false;
+        ActivatePropulsion.enabled = false;
     }
 
     public void Test()
